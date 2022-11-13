3b1b1cd3d962a1aec7c6892cd6a23ed7e3221a8f
IrynaTsymbaliuk <iryna.tsymbaliuk.dev@gmail.com>
2022-11-13 15:44:38 +0800

    Added material3 dependency

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling:1.2.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.2.1")
     implementation("androidx.compose.foundation:foundation:1.2.1")
-    implementation("androidx.compose.material:material:1.2.1")
+    implementation("androidx.compose.material3:material3:1.1.0-alpha02")
     implementation("androidx.activity:activity-compose:1.5.1")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
 }
