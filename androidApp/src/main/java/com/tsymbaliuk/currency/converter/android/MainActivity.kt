cbef6bcd782cdaa362449d2682f5c527e95226aa
IrynaTsymbaliuk <iryna.tsymbaliuk.dev@gmail.com>
2022-11-13 15:29:11 +0800

    Added Home screen

--- androidApp/src/main/java/com/tsymbaliuk/currency/converter/android/MainActivity.kt
+++ androidApp/src/main/java/com/tsymbaliuk/currency/converter/android/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.tsymbaliuk.currency.converter.Greeting
+import com.tsymbaliuk.currency.converter.android.ui.home.HomeScreen
 import com.tsymbaliuk.currency.converter.android.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting(Greeting().greeting())
+                    HomeScreen()
                 }
             }
         }
