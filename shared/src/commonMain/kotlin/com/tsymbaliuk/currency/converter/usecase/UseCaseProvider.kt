febb27d3f80ec1df4dba507884e28c5c68615235
IrynaTsymbaliuk <i.tsymbaliuk@geniusee.com>
2022-11-26 22:08:41 +0800

    Added FakeCurrencyRepositoryImpl

--- shared/src/commonMain/kotlin/com/tsymbaliuk/currency/converter/usecase/UseCaseProvider.kt
+++ shared/src/commonMain/kotlin/com/tsymbaliuk/currency/converter/usecase/UseCaseProvider.kt
@@ -1,10 +1,11 @@
 package com.tsymbaliuk.currency.converter.usecase
 
 import com.tsymbaliuk.currency.converter.repository.CurrencyRepository
+import com.tsymbaliuk.currency.converter.repository.FakeCurrencyRepositoryImpl
 
-class UseCaseProvider(
-    private val currencyRepository: CurrencyRepository
-) {
+class UseCaseProvider {
+
+    private val currencyRepository: CurrencyRepository = FakeCurrencyRepositoryImpl()
 
     val getCurrencyList: GetCurrencyList
         get() = GetCurrencyList(currencyRepository)
